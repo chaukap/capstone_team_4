eb1926ff851d8312c0929db1894ebdecd70db935
chaukap <chandlerhaukap@gmail.com>
2022-05-31 12:44:45 -0600

    Added documentation to the generate_count_query function

--- query_generator.py
+++ query_generator.py
@@ -1,6 +1,21 @@
 
 class query_generator:
     def generate_count_query(self, table, identifier_column, grouping_column, count_column):
+        """Generate an SQL query that returns the count of one column grouped by another.
+
+           Parameters
+           ----------
+           table : str,
+           identifier_column : str,
+           grouping_column : str,
+           count_column : str,
+           
+           Returns
+           ----------
+           A dict with two entries:
+           query: A string containing the SQL query that will satisfy the requirements.
+           multiple_contributors: A string containing an SQL query that will identify
+               users that contributed more than once to the results. """
         return {
             "query": f"""
                 SELECT {grouping_column}, {count_column}, COUNT({grouping_column})
