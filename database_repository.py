475fbd9398d4e5cf5fbd59ab7a4f39ca50c2a19a
chaukap <chandlerhaukap@gmail.com>
2022-06-08 16:14:12 -0600

    A very basic google authentication implementation

--- database_repository.py
+++ database_repository.py
@@ -22,29 +22,43 @@ class database_repository:
         if self.connection:
             self.connection.close()
 
+    def get_user(self, google_id):
+        cur = self.connection.cursor()
+        cur.execute(f"SELECT * FROM Users WHERE GoogleId = '{str(google_id)}'")
+        result = [n for n in cur]
+        if len(result) == 0:
+            return None
+        cur.close()
+        return result[0]
+
     def get_databases(self):
         cur = self.connection.cursor()
         cur.execute("SELECT * FROM ClientDatabases")
         result = [n for n in cur]
+        cur.close()
         return result
     
     def get_user_databases(self, user_id):
         cur = self.connection.cursor()
         cur.execute(f"SELECT * FROM ClientDatabases WHERE UserId = {str(user_id)}")
         result = [n for n in cur]
+        cur.close()
         return result
 
     def get_database_columns(self, database_id):
         cur = self.connection.cursor()
         cur.execute(f"SELECT * FROM ClientDatabaseColumns WHERE DatabaseId = {str(database_id)}")
         result = [n for n in cur]
+        cur.close()
         return result
 
-    def insert_user(self, email):
+    def insert_user(self, google_id, email):
         cur = self.connection.cursor()
-        cur.execute(f"INSERT INTO Users (Email) VALUES ({email})")
-        result = [n for n in cur]
-        return result
+        query = f"INSERT INTO Users (Email, GoogleId) VALUES ('{email}', '{str(google_id)}')"
+        cur.execute(query)
+        cur.close()
+        print(query)
+        return
     
     def insert_database(self, user_id, database_name, 
         host, username, password, table_name, 
@@ -59,8 +73,8 @@ class database_repository:
                 '{password}', {str(user_id)}, '{table_name}', 
                 '{public_name}', '{description}')
             """)
-        result = [n for n in cur]
-        return result
+        cur.close()
+        return
 
     def insert_database_column(self, database_id, name, max_bound, min_bound):
         cur = self.connection.cursor()
@@ -70,5 +84,5 @@ class database_repository:
             VALUES 
 	            ({database_id}, '{name}', {max_bound}, {min_bound});
             """)
-        result = [n for n in cur]
-        return result
+        cur.close()
+        return
--- main.py
+++ main.py
@@ -1,17 +1,65 @@
+from msilib.schema import ReserveCost
 from flask_sslify import SSLify
 from flask import Flask, make_response, request, redirect, render_template, url_for
 from mariadb_client import mariadb_client
 from differential_privacy_engine import differential_privacy_engine
 from helper_functions import check_form_fields
 from database_repository import database_repository
+import json
+from functools import wraps
+
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 app = Flask(__name__)
 sslify = SSLify(app)
+repo = database_repository()
 
-environment = "producion"
+f = open("keys.json")
+keys = json.load(f)
+f.close()
+GOOGLE_CLIENT_ID = keys["google_oauth"]["client_id"]
+ENVIRONMENT = keys["environment"]
+
+def authenticate(func):
+    @wraps(func)
+    def inner(*args, **kws):
+        jwt = request.cookies.get("gauth")
+        if jwt == None:
+            return redirect("/", 302)
+        try:
+            idinfo = id_token.verify_oauth2_token(jwt, requests.Request(), GOOGLE_CLIENT_ID)
+            userid = idinfo['sub']
+            user = repo.get_user(userid)
+            if user == None:
+                return make_response(redirect("/", 302))
+        except ValueError:
+            return redirect("https://www.youtube.com/watch?v=ZzWqfJFxC0w", code=302)
+        
+        return func(idinfo['email'], *args, **kws)
+    return inner
+
+def identify(func):
+    @wraps(func)
+    def inner(*args, **kws):
+        jwt = request.cookies.get("gauth")
+        if jwt == None:
+            return func(None, *args, **kws)
+        try:
+            idinfo = id_token.verify_oauth2_token(jwt, requests.Request(), GOOGLE_CLIENT_ID)
+            userid = idinfo['sub']
+            user = repo.get_user(userid)
+            if user == None:
+                return func(None, *args, **kws)
+        except ValueError:
+            return redirect("https://www.youtube.com/watch?v=ZzWqfJFxC0w", code=302)
+        
+        return func(idinfo['email'], *args, **kws)
+    return inner  
 
 @app.route('/query', methods=['POST'])
-def query():
+@authenticate
+def query(user_email):
     success, missing_field = check_form_fields([
         'database', 'username', 'password', 'port',
         'table', 'host', 'query_type', 'epsilon',
@@ -37,9 +85,10 @@ def query():
         response = make_response(values.to_csv())
         response.headers['Content-Disposition'] = "attachment; filename=results.csv"
         return response
-  
+
 @app.route('/table', methods=['POST'])
-def get_schema():
+@authenticate
+def get_schema(user_email):
     """Get the schema for a database table.
         Form Fields (Required)
         ----------
@@ -76,14 +125,44 @@ def get_schema():
         host=request.form['host'],
         database=request.form['database'],
         port=request.form['port'],
-        table=request.form['table'])
+        table=request.form['table'],
+        user_email = user_email)
 
 @app.route('/', methods=['GET'])
-def index():
+@identify
+def index(user_email):
+    if user_email != None:
+        return render_template("index.html", user_email = user_email)
     return render_template("index.html")
 
+@app.route("/login", methods=['POST'])
+def login():
+    csrf_token_cookie = request.cookies.get('g_csrf_token')
+    data = request.get_data(as_text=True)
+    data = data.replace('=', '":"').replace('&', '",\n"')
+    data = '{\n"' + data + '"\n}'
+    data = json.loads(data)
+
+    csrf_token_body = data['g_csrf_token']
+
+    if csrf_token_cookie != csrf_token_body:
+        return redirect("https://www.youtube.com/watch?v=ZzWqfJFxC0w", code=302)
+
+    try:
+        idinfo = id_token.verify_oauth2_token(data['credential'], requests.Request(), GOOGLE_CLIENT_ID)
+        userid = idinfo['sub']
+        user = repo.get_user(userid)
+        if user == None:
+            repo.insert_user(userid, idinfo['email'])
+
+        response = make_response(redirect("/", 302))
+        response.set_cookie("gauth", data['credential'])
+        return response
+    except ValueError:
+        return redirect("https://www.youtube.com/watch?v=ZzWqfJFxC0w", code=302)
+
 if __name__ == '__main__':
-    if environment == "production":
+    if ENVIRONMENT == "production":
         app.run()
     else:
         app.run(ssl_context='adhoc', debug=True)
